96e577ca5c05565ccadef9f4a1f42b61c0b25f3b
Alan Cutter <randfur@gmail.com>
2018-01-01 14:09:38 +1100

    sound.

--- main.js
+++ main.js
@@ -4,11 +4,36 @@ let canvas = document.getElementById('canvas');
 let context = canvas.getContext('2d');
 let toneCount = 128;
 let width = 0;
-let hegiht = 0;
+let height = 0;
+let audioContext = new AudioContext();
+let toneGains = createToneGains();
+
 
 function main() {
   resize();
-  // context.drawRect
+  toneGains[67].gain.setValueAtTime(0.5, 0);
+  toneGains[67].gain.setValueAtTime(0, 1);
+}
+
+function range(n) {
+  let result = [];
+  for (let i = 0; i < n; ++i) {
+    result.push(i);
+  }
+  return result;
+}
+
+function createToneGains() {
+  return range(toneCount).map(tone => {
+    let oscillator = audioContext.createOscillator();
+    oscillator.frequency.setValueAtTime(toneToFrequency(tone), 0);
+    let gain = audioContext.createGain();
+    gain.gain.setValueAtTime(0, 0);
+    oscillator.connect(gain);
+    gain.connect(audioContext.destination);
+    oscillator.start(0);
+    return gain;
+  });
 }
 
 function resize() {
